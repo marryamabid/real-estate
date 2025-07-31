4684152475b3480a2169f5183545148f487c7c80
Marryam Abid <marryam.abid11@gmail.com>
2025-08-01 00:39:53 +0500

    Header added

---
+++ client/src/Components/Header.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import { FaSearch } from "react-icons/fa";
+export default function Header() {
+  return (
+    <header className="bg-slate-200 shadow-md">
+      <nav className="flex justify-between items-center max-w-6xl mx-auto p-3">
+        <h1 className="font-bold text-sm sm:text-xl flex flex-wrap">
+          <span className="text-slate-500">Home</span>
+          <span className="text-slate-700">Linker</span>
+        </h1>
+
+        <form className="bg-slate-100 p-3 rounded-lg flex items-center">
+          <input
+            className="bg-transparent focus:outline-none w-24 sm:w-64"
+            type="text"
+            placeholder="Search..."
+            aria-label="search"
+          />
+          <button type="submit" aria-label="search-button">
+            <FaSearch />
+          </button>
+        </form>
+        <ul className="flex gap-4 text-xs sm:text-base">
+          <li className="hidden sm:inline text-slate-700 hover:underline">
+            Home
+          </li>
+          <li className="hidden sm:inline text-slate-700 hover:underline">
+            About
+          </li>
+          <li className="hidden sm:inline text-slate-700 hover:underline">
+            Profile
+          </li>
+          <li className=" text-slate-700 hover:underline"> Sign in</li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
---
+++ client/src/Pages/SignUp.jsx
@@ -0,0 +1,7 @@
+export default function SignUp() {
+  return (
+    <div>
+      <h2>Sign up</h2>
+    </div>
+  );
+}
