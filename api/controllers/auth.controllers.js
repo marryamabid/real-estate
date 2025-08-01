201205357de785a2c67d9ea7e502787fd97cde8b
Marryam Abid <marryam.abid11@gmail.com>
2025-08-01 21:31:46 +0500

    create middleware and error handler

--- api/controllers/auth.controllers.js
+++ api/controllers/auth.controllers.js
@@ -1,6 +1,6 @@
 import User from "../models/user.model.js";
 import bcrypt from "bcryptjs";
-export const authController = async (req, res) => {
+export const authController = async (req, res, next) => {
   const { username, email, password } = req.body;
   const bcryptSalt = await bcrypt.genSalt(10);
   const hashedPassword = await bcrypt.hash(password, bcryptSalt);
@@ -13,6 +13,6 @@ export const authController = async (req, res) => {
     await user.save();
     res.status(201).json("user created successfully");
   } catch (error) {
-    res.status(500).json(error.message);
+    next(error);
   }
 };
--- api/index.js
+++ api/index.js
@@ -14,8 +14,9 @@ mongoose
 const app = express();
 app.use(express.json());
 app.use("/api/auth", authRoutes);
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal Server Error";
+  return res.status(statusCode).json({ message, success: false, statusCode });
 });
-
 app.listen(3000);
